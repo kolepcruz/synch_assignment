f25a87a6e0445cc807edf25f4367019495cda019
Mr. Ian <ianloron00@gmail.com>
2021-05-14 15:58:05 -0300

    \0/ CmakeFiles

---
+++ CMakeFiles/synch_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/synch_assignment.dir/frontend/ss_view.cpp.o"
+  "CMakeFiles/synch_assignment.dir/frontend/ss_view.cpp.o.d"
+  "CMakeFiles/synch_assignment.dir/main.cpp.o"
+  "CMakeFiles/synch_assignment.dir/main.cpp.o.d"
+  "synch_assignment"
+  "synch_assignment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/synch_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
